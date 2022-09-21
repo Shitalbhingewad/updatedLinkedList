093f7b615ac30c4ca6a55473958f42aead44b685
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-09-21 14:12:19 +0530

    Welcome to Adressbook

---
+++ eclipse-workspace/constructday9/src/main/java/adressbook/constructday9/App.java
@@ -0,0 +1,10 @@
+package adressbook.constructday9;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to adressbook program" );
+    }
+}
