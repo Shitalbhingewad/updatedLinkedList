b29546b731a916ac15f0371eaec9e80eabdc10c1
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-09-29 15:36:14 +0530

    Play with 2 Player and finally report winner

--- eclipse-workspace/constructday4prob/src/main/java/com/snakeandladder/App.java
+++ eclipse-workspace/constructday4prob/src/main/java/com/snakeandladder/App.java
@@ -14,11 +14,12 @@ public class App {
 private static final Logger logger = LogManager.getLogger(App.class);
 
 public static void main(String[] args) {
-		logger.info("Welcome to Snake and Ladder Game ");
-		Player playObj=new Player();
-    	logger.info("Player's initial position :"+playObj.getPlayerPosition());
-
-    	playObj.play();
+	Player playObj1 = new Player();
+	Player playObj2 = new Player();
+	logger.info("Player's initial position is :" + playObj1.getPlayerPosition());
+	logger.info("Player's initial position is :" + playObj2.getPlayerPosition());
+	TwoPlayer two = new TwoPlayer();
+	two.playGame(playObj1, playObj2);
 }
 	}
 
