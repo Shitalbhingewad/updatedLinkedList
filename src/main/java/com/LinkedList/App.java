3f49537717d46a2fe3ffb8a5abbb4f12c82f77f0
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-11-25 17:44:57 +0530

    initialized LinkedList Problem with welcome message

---
+++ src/main/java/com/LinkedList/App.java
@@ -0,0 +1,10 @@
+package com.LinkedList;
+
+public class App {
+   
+
+    public static void main(String[] args) {
+       System.out.println("Hello to Linked List Practise Problem!");
+       
+    }
+}
